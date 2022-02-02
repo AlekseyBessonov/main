38a22ccd199764dda544efc59bac29c0733d8c68
AlekseyBessonov <94831046+AlekseyBessonov@users.noreply.github.com>
2022-02-02 16:27:21 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
 currency = {'доллар': 'USD',
             'евро': 'EUR',
-            'рубль': 'RUB'}
+            'рубль': 'RUB',
+             'реал': 'BRL',
+            'фунт': 'GBP',
+            'донг': 'VND',
+            'крона': 'DKK',
+            'песо':'MXN',
+            'шекель': 'ILS',
+            'рупия': 'INR',
+            'бат' : 'THB',
+            'лира' : 'TRY',
+            'гривна': 'UAH',
+            'юань' : 'CNY'}
 
 TOKEN = "5205381134:AAGhishbPSmtJRfhI13bqeedvnTM6tdeCzo"
